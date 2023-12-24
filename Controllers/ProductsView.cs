b5f09a444672fc094027fb59f70f915776bc9f46
nhatanh <nhatanhltr@gmail.com>
2023-12-24 16:18:26 +0700

    djalk

---
+++ Controllers/ProductsView.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using startup.Models;
+
+namespace startup.Controllers
+{
+    public class ProductsView : Controller
+    {
+        private readonly DataContext _context;
+
+        public ProductsView(DataContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet("/slug")]
+        public IActionResult Details(string slug)
+        {
+            if(slug == null) { return NotFound(); };
+            var pr = _context.products.FirstOrDefault(x => x.Slug == slug);
+            return View(pr);
+
+        }
+    }
+}
